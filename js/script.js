2ce83b6abcbd0c400c54b63c70a6122b3cd6fc50
collo <qollomcbizzy64@gmail.com>
2017-05-12 17:14:02 +0300

    display the results when the user has clicked or rolled

--- js/script.js
+++ js/script.js
@@ -4,12 +4,20 @@ function Dice(dice1,dice2){
   this.number2=dice2;
 }
 Dice.prototype.totalMarks = function () {
+  if(this.number1 === 1 || this.number2 === 1){
+    return   "you have lost";
+  }
+  else {
   return this.number1 + this.number2;
+}
 };
 //user interface
 $(document).ready(function(){
   $("button#player1").click(function(event){
     event.preventDefault();
+    $("span.remove1").remove();
+    $("span.removediceno1").remove();
+    $("span.removediceno2").remove();
     var inputedDice1=$('#p1dice1').attr('id');
     var inputedDice2=$('#p1dice2').attr('id');
     var d1=Math.floor(Math.random() * 6) +1;
@@ -17,10 +25,15 @@ $(document).ready(function(){
     inputedDice1 = d1;
     inputedDice2 = d2;
     var newDice= new Dice(inputedDice1,inputedDice2);
-    $("#output1").append(newDice.totalMarks());
+    $("#output1").append("<span class='remove1'>" + newDice.totalMarks() + "</span>");
+    $("span#di1").append("<span class='removediceno1'>" + d1 + "</span>");
+    $("span#di2").append("<span class='removediceno2'>" + d2 + "</span>");
   });
   $("button#player2").click(function(event){
     event.preventDefault();
+    $("span.remove2").remove();
+    $("span.removedice1").remove();
+    $("span.removedice2").remove();
     var inputedDice1=$('#p2dice1').attr('id');
     var inputedDice2=$('#p2dice2').attr('id');
     var d1=Math.floor(Math.random() * 6) +1;
@@ -28,6 +41,8 @@ $(document).ready(function(){
     inputedDice1 = d1;
     inputedDice2 = d2;
     var newDice= new Dice(inputedDice1,inputedDice2);
-  $("#output2").append(newDice.totalMarks());
+  $("#output2").append("<span class='remove2'>" + newDice.totalMarks() + "</span>");
+  $("span#d1").append("<span class='removedice1'>" + d1 + "</span>");
+  $("span#d2").append("<span class='removedice2'>" + d2 + "</span>");
 });
 });
--- readme.md
+++ readme.md
@@ -8,3 +8,4 @@ It is a game that gives the two users to play against each other on rolling the 
  3. Create a user interface to receive the input from the rolled number
  4. Create a constructor to hold different dices played
  5. Create a prototype to calculate the total marks
+ 6. show the total marks after somebody has displayed
