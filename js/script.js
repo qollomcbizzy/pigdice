9056b1c0977de49443ffb1319d2e4c6baabec592
collo <qollomcbizzy64@gmail.com>
2017-05-12 15:08:40 +0300

    display the results when the user has clicked or rolled

--- js/script.js
+++ js/script.js
@@ -8,14 +8,26 @@ Dice.prototype.totalMarks = function () {
 };
 //user interface
 $(document).ready(function(){
-  $("button#click").click(function(event){
+  $("button#player1").click(function(event){
     event.preventDefault();
-    var inputedDice1=$(document).getElementById('dice1');
-    var inputedDice2=$(document).getElementById('dice2');
+    var inputedDice1=$('#p1dice1').attr('id');
+    var inputedDice2=$('#p1dice2').attr('id');
     var d1=Math.floor(Math.random() * 6) +1;
     var d2=Math.floor(Math.random() * 6) +1;
     inputedDice1 = d1;
     inputedDice2 = d2;
     var newDice= new Dice(inputedDice1,inputedDice2);
-  })
+    $("#output1").append(newDice.totalMarks());
+  });
+  $("button#player2").click(function(event){
+    event.preventDefault();
+    var inputedDice1=$('#p2dice1').attr('id');
+    var inputedDice2=$('#p2dice2').attr('id');
+    var d1=Math.floor(Math.random() * 6) +1;
+    var d2=Math.floor(Math.random() * 6) +1;
+    inputedDice1 = d1;
+    inputedDice2 = d2;
+    var newDice= new Dice(inputedDice1,inputedDice2);
+  $("#output2").append(newDice.totalMarks());
+});
 });
