aa69a4b58e7477b5a3d26b71319d32fa281598e0
collo <qollomcbizzy64@gmail.com>
2017-05-12 14:26:49 +0300

    created the user interface

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+//back-end logic
 function Dice(dice1,dice2){
   this.number1=dice1;
   this.number2=dice2;
 }
+//user interface
+$(document).ready(function(){
+  $("button#click").click(function(event){
+    event.preventDefault();
+    var inputedDice1=$(document).getElementById('dice1');
+    var inputedDice2=$(document).getElementById('dice2');
+    var d1=Math.floor(Math.random() * 6) +1;
+    var d2=Math.floor(Math.random() * 6) +1;
+    inputedDice1 = d1;
+    inputedDice2 = d2;
+  })
+});
--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@ It is a game that gives the two users to play against each other on rolling the 
 ####    PROJECT SPECIFICATION
  1. Create an object dice on the back-end
  2. Create an html for the input type and the css
+ 3. Create a user interface to receive the input from the rolled number
