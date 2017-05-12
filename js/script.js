c037304ce541e59cbc6bec66e8d6cb749974c8f1
collo <qollomcbizzy64@gmail.com>
2017-05-12 17:53:53 +0300

    added if condition statement to display the results if he/she has won

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ Dice.prototype.totalMarks = function () {
   if(this.number1 === 1 || this.number2 === 1){
     return   "you have lost";
   }
+  else if((this.number1 + this.number2) === 40 ) {
+    return  "congratulation you have won !!";
+  }
   else {
   return this.number1 + this.number2;
 }
@@ -20,8 +23,8 @@ $(document).ready(function(){
     $("span.removediceno2").remove();
     var inputedDice1=$('#p1dice1').attr('id');
     var inputedDice2=$('#p1dice2').attr('id');
-    var d1=Math.floor(Math.random() * 6) +1;
-    var d2=Math.floor(Math.random() * 6) +1;
+    var d1=Math.floor(Math.random() * 30) +1;
+    var d2=Math.floor(Math.random() * 30) +1;
     inputedDice1 = d1;
     inputedDice2 = d2;
     var newDice= new Dice(inputedDice1,inputedDice2);
@@ -36,8 +39,8 @@ $(document).ready(function(){
     $("span.removedice2").remove();
     var inputedDice1=$('#p2dice1').attr('id');
     var inputedDice2=$('#p2dice2').attr('id');
-    var d1=Math.floor(Math.random() * 6) +1;
-    var d2=Math.floor(Math.random() * 6) +1;
+    var d1=Math.floor(Math.random() * 30) +1;
+    var d2=Math.floor(Math.random() * 30) +1;
     inputedDice1 = d1;
     inputedDice2 = d2;
     var newDice= new Dice(inputedDice1,inputedDice2);
